6f12b11ca0c9194b41bd527286618022c2b9c18c
dbstj0403 <wcn09088@naver.com>
2024-07-04 10:45:43 +0900

    [Environment] Prettier/ESLint 충돌되는 설정 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
+/*
+  경로 설정은 여기에서 합니다! 자신이 만들 경로들 위에 주석으로 이름 표기 부탁드립니다. 🙇‍♀️
+// 페이지 url은 모두 소문자로 작성합니다.
+*/
 function App() {
   return (
     <div>
       <h1>홍익대학교 멋쟁이사자처럼 2024 대동제 프로젝트입니다. 🦁</h1>
       <Example>컴끼얏호우</Example>
+      {/* 😽 윤서 ---------- */}
     </div>
   );
 }
