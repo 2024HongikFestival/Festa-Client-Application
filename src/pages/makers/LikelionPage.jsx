7ea6716b467c7a64149758a025b25b10b817775e
letthem <suyeon3662@naver.com>
2024-08-12 02:01:15 +0900

    [Feat] 개화 페이지 1차 퍼블리싱

--- src/pages/makers/LikelionPage.jsx
+++ src/pages/makers/LikelionPage.jsx
@@ -4,7 +4,7 @@ import LikelionCard from './LikelionCard';
 import { useTranslation } from 'react-i18next';
 
 const LikelionPage = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <LikelionLayout>
