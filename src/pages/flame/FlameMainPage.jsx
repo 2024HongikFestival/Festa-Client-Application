4fb304a8f5370800ef5f5be871c4a396eb36ec36
chaeyoungwon <epopcy@naver.com>
2024-08-15 23:40:25 +0900

    [Design] 와디페 메인화면 디자인 수정

--- src/pages/flame/FlameMainPage.jsx
+++ src/pages/flame/FlameMainPage.jsx
@@ -4,6 +4,7 @@ import Header from '@/components/layouts/Header';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import FLAME from '../../assets/svgs/FLAME.svg';
+import speaker from '../../assets/speaker.webp';
 
 const FlameMainPage = () => {
   const [selectedDay, setSelectedDay] = useState('day1');
@@ -13,10 +14,16 @@ const FlameMainPage = () => {
       <Flame>
         <Title>WOW DJ FESTIVAL</Title>
         <WDFLogo src={FLAME} alt="flame" />
-        <WDFImage></WDFImage>
+        {/* <WDFImage src={speaker}></WDFImage> */}
         <WDFContainer>
-          <WDFIntroduction>ONE AND ONLY</WDFIntroduction>
-          <WDFIntroduction>와우 디제이 페스티벌</WDFIntroduction>
+          <WDFIntroduction>
+            <IntroWrapper>
+              <Introduction>ONE&nbsp;</Introduction>
+              <Introduction1>AND&nbsp;</Introduction1>
+              <Introduction> ONLY</Introduction>
+            </IntroWrapper>
+            <WDFName>와우 디제이 페스티벌</WDFName>
+          </WDFIntroduction>
           <WDFDescription>
             현실의 무게와 무력감에 깨져가는 청춘을 깨우고 <br />
             가장 아름답게 빛나는 &apos;나&apos;를 마주할 시간입니다. <br />
@@ -27,13 +34,13 @@ const FlameMainPage = () => {
 
         <DateSection>
           <DateButton onClick={() => setSelectedDay('day1')}>
-            Day 1 <br /> 9.25 (수)
+            DAY 1 <br /> 9.25 (수)
           </DateButton>
           <DateButton onClick={() => setSelectedDay('day2')}>
-            Day 2 <br /> 9.26 (목)
+            DAY 2 <br /> 9.26 (목)
           </DateButton>
           <DateButton onClick={() => setSelectedDay('day3')}>
-            Day 3<br /> 9.27 (금)
+            DAY 3<br /> 9.27 (금)
           </DateButton>
         </DateSection>
 
@@ -90,42 +97,86 @@ const WDFLogo = styled.img``;
 const WDFContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   justify-content: center;
+  gap: 0.75rem;
+`;
+
+const WDFIntroduction = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  width: 11.313rem;
 `;
 
 const WDFImage = styled.img`
   display: flex;
-  width: 16.5rem;
-  height: 20.313rem;
-  margin-bottom: 1.25rem;
+  width: 16.125rem;
+  height: 22.563rem;
 `;
-const WDFIntroduction = styled.span`
-  font-size: 1.125rem;
-  font-weight: 600;
+
+const Introduction = styled.span`
+  ${(props) => props.theme.fontStyles.basic.body2Bold};
+  font-size: 0.875rem;
+  font-weight: 700;
+  background: linear-gradient(281.55deg, #fcf661 1.09%, #ff0e2b 38.8%, #df2020 62.66%, #bc1700 96.55%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-shadow: 0px 0px 8px rgba(255, 255, 255, 0.25);
+  color: transparent;
+  text-align: center;
+`;
+const Introduction1 = styled.span`
+  ${(props) => props.theme.fontStyles.basic.body2Bold};
+  font-size: 0.875rem;
+  text-align: center;
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.gray40};
+  text-shadow: 0px 0px 8px rgba(255, 255, 255, 0.25);
+`;
+
+const WDFName = styled.span`
+  ${(props) => props.theme.fontStyles.flame.headline6};
+  color: ${(props) => props.theme.colors.white};
+  font-size: 1.25rem;
+  text-align: center;
+  font-weight: 900;
+  text-shadow: 0px 0px 8px rgba(255, 255, 255, 0.25);
 `;
 
 const WDFDescription = styled.span`
-  margin-top: 1.25rem;
+  text-align: center;
+  ${(props) => props.theme.fontStyles.basic.body2Med};
+  color: ${(props) => props.theme.colors.gray60};
   font-size: 0.875rem;
-  font-weight: 400;
+  padding-bottom: 3.25rem;
 `;
 
 const DateSection = styled.div`
   display: flex;
-  gap: 1rem;
-  margin: 1rem 0;
-  margin-top: 4.938rem;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+  padding-top: 2rem;
+  margin-top: 1.75rem;
 `;
 
 const DateButton = styled.button`
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
+  ${(props) => props.theme.fontStyles.basic.subHeadBold};
+  color: ${(props) => props.theme.colors.gray80};
+  font-size: 1.125rem;
   cursor: pointer;
   border: none;
+  letter-spacing: -0.001rem;
 
   &:hover {
-    font-weight: 600;
+    background: linear-gradient(103.64deg, #df2020 35.72%, #ffb800 88.25%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    color: transparent;
   }
 `;
 
@@ -135,4 +186,11 @@ const DayContent = styled.div`
 
 const Content = styled.div`
   padding: 1rem;
+`;
+
+const IntroWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
