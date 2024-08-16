fda6a718e00f6be600014c73ab01ec7577b2c39a
ocahs9 <piorag99@gmail.com>
2024-08-16 19:20:18 +0900

    [Feat] 게시글 단건 조회 api 연결 완료

--- src/pages/lost-and-found/pages/LostAndFoundDetail/LostAndFoundDetail.jsx
+++ src/pages/lost-and-found/pages/LostAndFoundDetail/LostAndFoundDetail.jsx
@@ -1,10 +1,32 @@
 // 대동제 분실물 (상세)
 // url: /lost-and-found/{post-id}
 
-// 이 페이지는 수정 필요하면 이름이나 위치는 바꾸셔도 됩니당 (url은 고정)
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const LostAndFoundDetail = () => {
+  const { lostId } = useParams();
+  const [item, setItem] = useState({});
+
+  const getItemApi = async () => {
+    try {
+      //전체 데이터를 받고 싶다면 params 안 넘겨도 됨 - 필터링 구현 때 로직 구체화하기
+      const response = await axios.get(`https://api.2024hongikfestival.com/losts/${lostId}`);
+      setItem(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getItemApi();
+  }, []);
+
+  useEffect(() => {
+    console.log(item);
+  }, [item]);
+
   return <div>LostAndFoundDetail</div>;
 };
 
