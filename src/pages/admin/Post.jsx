f0aa6b7c9df347c6361501819a8008b67726fd11
chaeyoungwon <epopcy@naver.com>
2024-08-17 04:49:14 +0900

    [Fix] 분실물 게시글 year 추가

--- src/pages/admin/Post.jsx
+++ src/pages/admin/Post.jsx
@@ -156,12 +156,13 @@ const Post = ({ posts, userId, setIsDetailView, setPostId }) => {
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
+    const year = String(date.getFullYear()).padStart(4, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
-    return `${month}.${day} ${hours}:${minutes}:${seconds}`;
+    return `${year}.${month}.${day} ${hours}:${minutes}:${seconds}`;
   };
 
   const OptionsMenu = ({ onUndo, isDeleted, onBlockAndDelete }) => (
