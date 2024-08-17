1309787115dc644cd93eb7d48689b4b776263d77
chaeyoungwon <epopcy@naver.com>
2024-08-18 01:37:58 +0900

    [Fix] 컴포넌트 전체 높이 수정

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -82,7 +82,7 @@ export default AdminPage;
 
 const Container = styled.div`
   background-color: ${(props) => props.theme.colors.gray10};
-  height: 100%;
+  height: 100vh;
 `;
 
 const SelectBar = styled.div`
