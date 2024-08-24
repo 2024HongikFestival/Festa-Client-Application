c1933d562ce3a8757d4795a61b093c33d435d34d
ocahs9 <piorag99@gmail.com>
2024-08-24 23:43:27 +0900

    [Feat] 카메라 기능 구현

--- src/pages/lost-and-found/pages/AddLostItem/AddLostItem.jsx
+++ src/pages/lost-and-found/pages/AddLostItem/AddLostItem.jsx
@@ -1,10 +1,10 @@
 // 대동제 분실물 (제보 글 작성 페이지)
 // url: /lost-and-found/add
-
 import React from 'react';
+import CameraPage from './components/Camera/CameraPage';
 
 const AddLostItem = () => {
-  return <div>AddLostItem</div>;
+  return <CameraPage />;
 };
 
 export default AddLostItem;
