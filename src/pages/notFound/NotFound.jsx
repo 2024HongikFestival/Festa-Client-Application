5cc03ad212cdc4c45924515573c6904b8cd9e309
서가영 <alstpgml0812@gmail.com>
2024-08-29 18:15:30 +0900

    [Fix] 404 멘트 수정

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -22,7 +22,7 @@ export default function NotFoundPage() {
           <p>
             찾으려는 페이지의 주소가 잘못 입력되었거나,
             <br />
-            주소의 변경 삭제로 인해 사용하실 수 없습니다
+            주소 변경 또는 삭제로 인해 사용하실 수 없습니다
           </p>
         </S.BottomText>
       </S.Text>
