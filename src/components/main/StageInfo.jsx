fa6f6267219ce1a4f4171458c765fd5235d7f463
dbstj0403 <wcn09088@naver.com>
2024-08-30 02:24:01 +0900

    [Design] Create Main page stage component

---
+++ src/components/main/StageInfo.jsx
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import PieceImg from '@/assets/svgs/main/piece.svg';
+export default function StageInfo() {
+  return (
+    <Container>
+      <Date>
+        <Day>DAY 1</Day>
+        <MonthAndDay>9.25</MonthAndDay>
+      </Date>
+      <BubbleWrapper>
+        <Piece src={PieceImg} alt="" />
+        <Bubble>
+          <TextContainer>
+            <TextWrapper>
+              <Text>학부 찬조 공연</Text>
+              <Text time>17:30 - 18:00</Text>
+            </TextWrapper>
+            <TextWrapper>
+              <Text>학부 찬조 공연</Text>
+              <Text time>17:30 - 18:00</Text>
+            </TextWrapper>
+            <TextWrapper>
+              <Text>학부 찬조 공연</Text>
+              <Text time>17:30 - 18:00</Text>
+            </TextWrapper>
+          </TextContainer>
+        </Bubble>
+      </BubbleWrapper>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 33.475rem;
+  height: 13.5rem;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1.6rem;
+`;
+
+const Date = styled.div`
+  width: 3.5rem;
+  height: 4.9rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Day = styled.p`
+  ${(props) => props.theme.fontStyles.basic.captionMed};
+  color: ${(props) => props.theme.colors.gray80};
+`;
+
+const MonthAndDay = styled.p`
+  ${(props) => props.theme.fontStyles.basic.subHeadBold};
+  color: ${(props) => props.theme.colors.gray80};
+`;
+
+const BubbleWrapper = styled.div`
+  width: 28.675rem;
+  height: 13.5rem;
+  display: flex;
+`;
+
+const Piece = styled.img`
+  width: 1.975rem;
+  height: 2.7rem;
+  margin-top: 1rem;
+`;
+
+const Bubble = styled.div`
+  width: 26.7rem;
+  height: 13.5rem;
+  background-color: white;
+  border-radius: 1.2rem;
+  box-shadow: 0 0 0.8rem 0 rgba(0, 0, 0, 0.12);
+  padding: 2.4rem;
+`;
+
+const TextContainer = styled.div`
+  width: 21.9rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
+
+const TextWrapper = styled.div`
+  width: 21.9rem;
+  height: 2.1rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Text = styled.p`
+  ${(props) => props.theme.fontStyles.basic.body2Bold};
+  color: ${(props) => (props.time ? props.theme.colors.gray60 : props.theme.colors.gray70)};
+`;
--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import MoveToWdfBtn from '@/components/main/MoveToWdfBtn';
 import { useTranslation } from 'react-i18next';
 import mainImg from '@/assets/webps/main/mainExample.webp';
+import StageInfo from '@/components/main/StageInfo';
 export default function MainPage() {
   // const { t, i18n } = useTranslation();
 
@@ -13,26 +14,28 @@ export default function MainPage() {
 
   return (
     <Container>
-      <Title>
-        2024
-        <br />
-        홍익대동제
-      </Title>
-      <Desc>
-        활짝 피어있는 지금
-        <br />
-        있는 그대로 만개한 청춘인 이 순간을
-        <br />
-        2024 화양연화 ; 만개로 하여금 기록되어
-        <br />
-        오랫동안 기억될 것입니다.
-      </Desc>
-      <Img src={mainImg} alt="main" />
+      <Wrapper>
+        <Title>
+          2024
+          <br />
+          홍익대동제
+        </Title>
+        <Desc>
+          활짝 피어있는 지금
+          <br />
+          있는 그대로 만개한 청춘인 이 순간을
+          <br />
+          2024 화양연화 ; 만개로 하여금 기록되어
+          <br />
+          오랫동안 기억될 것입니다.
+        </Desc>
+        <Img src={mainImg} alt="main" />
+      </Wrapper>
       <LineupTitleWrapper>
         <Date>9.25 (수)</Date>
         <LineupTitle>오늘의 라인업</LineupTitle>
       </LineupTitleWrapper>
-      <LineupInfoWrapper>
+      {/* <LineupInfoWrapper>
         <LineupCompWrapper>
           <LineupComp height={'long'}>
             <LineupText>이름텍스트영역</LineupText>
@@ -49,8 +52,17 @@ export default function MainPage() {
             <LineupText>이름텍스트영역</LineupText>
           </LineupComp>
         </LineupCompWrapper>
-      </LineupInfoWrapper>
+      </LineupInfoWrapper> */}
+      <LineupInfoWrapper></LineupInfoWrapper>
       <GoLineupPageBtn>전체 라인업 보러가기 {'>'}</GoLineupPageBtn>
+      <Hr />
+      <StageContainer>
+        <StageTitle>중앙 무대 일정</StageTitle>
+        <StageInfo />
+        <StageInfo />
+        <StageInfo />
+        <GoHibsBtn />
+      </StageContainer>
     </Container>
   );
 }
@@ -64,6 +76,14 @@ export default function MainPage() {
 }
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -100,6 +120,7 @@ const LineupTitleWrapper = styled.div`
 
 const Date = styled.p`
   ${(props) => props.theme.fontStyles.basic.subHeadBold}
+  color: ${(props) => props.theme.colors.gray10};
   text-align: center;
 `;
 
@@ -110,8 +131,12 @@ const LineupTitle = styled.p`
 
 const LineupInfoWrapper = styled.div`
   width: 33.5rem;
+  height: 50.9rem;
   display: flex;
   justify-content: space-between;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 1.2rem;
+  border: 1px solid black;
 `;
 
 const LineupCompWrapper = styled.div`
@@ -137,6 +162,7 @@ const GoLineupPageBtn = styled.button`
   height: 5.4rem;
   box-shadow: 0px 0px 0.8rem 0 rgba(0, 0, 0, 0.12);
   ${(props) => props.theme.fontStyles.basic.headline5}
+  margin-bottom: 6.4rem;
 `;
 
 const LineupText = styled.div`
@@ -146,4 +172,55 @@ const LineupText = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const Hr = styled.div`
+  width: 100%;
+  height: 1.2rem;
+  background-color: rgba(160, 227, 255, 0.5);
+`;
+
+const StageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 6.4rem;
+`;
+
+const StageTitle = styled.div`
+  ${(props) => props.theme.fontStyles.main.headline3}
+  margin-bottom: 2.8rem;
+`;
+
+const GoHibsBtn = styled.button`
+  width: 33.2rem;
+  height: 7.8rem;
+  margin-top: 3.6rem;
+  border: none;
+  border-radius: 1.2rem;
+  background: linear-gradient(91.85deg, rgba(3, 124, 158, 0.32) 5.35%, rgba(22, 170, 234, 0.32) 102.2%);
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 1.2rem;
+    padding: 0.2rem;
+    background: linear-gradient(180deg, rgba(199, 255, 208, 0.56) 0%, rgba(79, 181, 255, 0.2) 100%);
+    mask:
+      linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    mask-composite: exclude;
+    -webkit-mask-composite: xor;
+    pointer-events: none;
+  }
+
+  box-shadow: 0 0 0.8rem 0 rgba(0, 0, 0, 0.12);
 `;
