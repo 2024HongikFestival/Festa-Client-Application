7f5cd43b08881dbd33186e9aaecb408211f91a78
dong-wook <boogiewooki02@gmail.com>
2024-09-05 22:43:10 +0900

    [Refactor] getSelectedDayByDate함수 생성

--- src/pages/stage/LineupPage.jsx
+++ src/pages/stage/LineupPage.jsx
@@ -14,29 +14,34 @@ import {
   Title,
 } from './styles/LineupPage';
 
+// 날짜에 따른 Day 선택 함수
+const getSelectedDayByDate = () => {
+  const today = new Date();
+  const month = today.getMonth() + 1; // 월 (0부터 시작하므로 1 더함)
+  const day = today.getDate(); // 일
+
+  // 날짜에 따른 Day 선택
+  if (month === 9) {
+    switch (day) {
+      case 25:
+        return 'Day1';
+      case 26:
+        return 'Day2';
+      case 27:
+        return 'Day3';
+      default:
+        return 'Day1'; // 9월 25-27일이 아닌 경우 Day1으로 설정
+    }
+  } else {
+    return 'Day1'; // 9월이 아닌 경우 Day1으로 설정
+  }
+};
+
 const LineupPage = () => {
   const [selectedDay, setSelectedDay] = useState('Day1');
 
   useEffect(() => {
-    // 현재 날짜 가져오기
-    const today = new Date();
-    const month = today.getMonth() + 1; // 월 (0부터 시작하므로 1 더함)
-    const day = today.getDate(); // 일
-
-    // 날짜에 따른 Day 선택
-    if (month === 9) {
-      if (day === 25) {
-        setSelectedDay('Day1');
-      } else if (day === 26) {
-        setSelectedDay('Day2');
-      } else if (day === 27) {
-        setSelectedDay('Day3');
-      } else {
-        setSelectedDay('Day1'); // 9월 25-27일이 아닌 경우 Day1으로 설정
-      }
-    } else {
-      setSelectedDay('Day1'); // 9월이 아닌 경우 Day1으로 설정
-    }
+    setSelectedDay(getSelectedDayByDate());
   }, []);
 
   // 수정 예정
