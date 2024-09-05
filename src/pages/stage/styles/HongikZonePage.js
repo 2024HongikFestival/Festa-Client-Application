689961d4bb420baa099475b6e26b14b7bbf70da8
dong-wook <boogiewooki02@gmail.com>
2024-09-05 14:57:54 +0900

    [Feat] 홍익존 1차 퍼블리싱

--- src/pages/stage/styles/HongikZonePage.js
+++ src/pages/stage/styles/HongikZonePage.js
@@ -25,6 +25,7 @@ export const BoxContainer = styled.div`
   gap: 2.8rem;
 `;
 export const Info = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -34,6 +35,60 @@ export const Info = styled.div`
   border-radius: 1.6rem;
   background: ${(props) => props.theme.colors.white};
 `;
+export const LocationBox = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 2.4rem 0 2.6rem 0;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-self: stretch;
+  gap: 1.6rem;
+  border-radius: 1.6rem;
+  background: ${(props) => props.theme.colors.white};
+`;
 export const TextBox = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.2rem;
+  align-self: stretch;
+`;
+export const SubTitle = styled.p`
+  color: ${(props) => props.theme.colors.hongikBlue};
+  ${(props) => props.theme.fontStyles.basic.subHeadBold};
+
+  &.gray100 {
+    color: ${(props) => props.theme.colors.gray100};
+  }
+`;
+export const List = styled.ul`
+  list-style-type: disc;
+  padding-left: 1.9rem;
+  li {
+    ${(props) => props.theme.fontStyles.basic.body2Med};
+    color: ${(props) => props.theme.colors.gray70};
+    margin-bottom: 0.4rem;
+  }
+  li:last-child {
+    margin-bottom: 0;
+  }
+`;
+export const BlackHighlight = styled.span`
+  ${(props) => props.theme.fontStyles.basic.body2Bold};
+  color: ${(props) => props.theme.colors.gray70};
+`;
+export const BlueHighlight = styled.span`
+  ${(props) => props.theme.fontStyles.basic.body2Bold};
+  color: ${(props) => props.theme.colors.hongikBlue};
+`;
+export const LocationTitle = styled.p`
+  ${(props) => props.theme.fontStyles.main.headline6};
+  color: ${(props) => props.theme.colors.gray100};
+`;
+export const Map = styled.div`
+  width: 33.5rem;
+  height: 25rem;
+  border-top: 1px solid ${(props) => props.theme.colors.gray30};
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray30};
 `;
