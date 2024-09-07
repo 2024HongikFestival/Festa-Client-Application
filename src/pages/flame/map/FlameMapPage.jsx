fd088eead17d422a2e9827cd935c28728afe69bc
Cuyqo <saon0410@gmail.com>
2024-09-07 22:08:37 +0900

    [Design] 로고 수정

--- src/pages/flame/map/FlameMapPage.jsx
+++ src/pages/flame/map/FlameMapPage.jsx
@@ -1,19 +1,61 @@
 // 와디페 사이트맵
 // url: /flame/map
-import { BoothBox, BoothOne, MapBox, MapImgBox, MapTitle, MapWrapper } from './styles.js';
+import { BoothBox, BoothIntro, BoothOne, LottieBox, MapBox, MapImgBox, MapTitle, MapWrapper } from './styles.js';
 import React from 'react';
 import mapImg from '/src/assets/webps/map/sitemap.webp';
+import Lottie from 'react-lottie';
+import arrowAnimation from './siteMap.json';
+import gmLogo from '/src/assets/webps/map/gm.webp';
+import masterLogo from '/src/assets/webps/map/master.webp';
+import redBullLogo from '/src/assets/webps/map/redBull.webp';
+import snolLogo from '/src/assets/webps/map/image02_1.webp';
 
 const FlameMapPage = () => {
+  const arrowMotion = {
+    loop: true,
+    autoplay: true,
+    animationData: arrowAnimation,
+  };
   return (
     <MapWrapper>
       <MapTitle>사이트맵</MapTitle>
-      <MapImgBox src={mapImg} />
+      <MapBox>
+        <MapImgBox src={mapImg} />
+        <LottieBox>
+          <Lottie className="arrow" options={arrowMotion} margin={0} />
+        </LottieBox>
+      </MapBox>
       <BoothBox>
-        <BoothOne>A. 금군양조</BoothOne>
-        <BoothOne>B. 레드불</BoothOne>
-        <BoothOne>C. 스놀</BoothOne>
-        <BoothOne>D. 예거마이스터</BoothOne>
+        <BoothOne>
+          <BoothIntro>
+            <p className="name">A. 입장부스 (프루틱스)</p>
+            <p className="intro">프루틱스 상품 제공</p>
+          </BoothIntro>
+        </BoothOne>
+        <BoothOne>
+          <BoothIntro>
+            <p className="name">B. 레드불</p>
+            <p className="intro">슈가로로 상품 제공</p>
+          </BoothIntro>
+        </BoothOne>
+        <BoothOne>
+          <BoothIntro>
+            <p className="name">C. 예거 마이스터</p>
+            <p className="intro">슈가로로 상품 제공</p>
+          </BoothIntro>
+        </BoothOne>
+        <BoothOne>
+          <BoothIntro>
+            <p className="name">D. 금군양조</p>
+            <p className="intro">슈가로로 상품 제공</p>
+          </BoothIntro>
+        </BoothOne>
+        <BoothOne>
+          <BoothIntro>
+            <p className="name">E. 스놀</p>
+            <p className="intro">슈가로로 상품 제공</p>
+          </BoothIntro>
+        </BoothOne>
       </BoothBox>
     </MapWrapper>
   );
--- src/pages/flame/map/styles.js
+++ src/pages/flame/map/styles.js
@@ -1,4 +1,15 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(2rem);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 export const MapWrapper = styled.div`
   padding: 2.4rem 0 6.4rem;
@@ -14,8 +25,13 @@ export const MapTitle = styled.div`
 export const MapBox = styled.div`
   width: calc(100% - 4rem);
   margin: 2.8rem 2rem 4rem;
-  height: 263px;
-  border: 0.1rem solid saddlebrown;
+  position: relative;
+  display: flex;
+  justify-content: center;
+
+  .arrow {
+  }
+
   /* border-image: linear-gradient(0deg, rgba(255, 255, 255, 0.25) 20%, rgba(223, 33, 33, 0.8) 100%) 1;
   box-shadow: 0px 0px 8px 0px rgba(255, 255, 255, 0.25); */
   /* 와디페 line shadow */
@@ -27,17 +43,55 @@ export const BoothBox = styled.div`
   width: calc(100% - 4rem);
   margin: 0 2rem 0;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
-  gap: 2.8rem;
+  gap: 2.4rem;
 `;
 
 export const BoothOne = styled.div`
   ${(props) => props.theme.fontStyles.basic.body2Med};
   color: ${(props) => props.theme.colors.white};
+  display: flex;
+  justify-content: space-between;
+  animation: ${fadeInUp} 0.8s ease-out;
+
+  gap: 4.4rem;
+  img {
+    width: 77px;
+  }
+
+  .snol {
+    color: white;
+  }
 `;
 
 export const MapImgBox = styled.img`
-  width: calc(100% - 4rem);
-  margin: 2.8rem 2rem 4rem;
+  width: 33.5rem;
+  max-width: 33.5rem;
   height: 26.3rem;
+`;
+
+export const LottieBox = styled.div`
+  width: 33.5rem;
+  position: absolute;
+  top: 0;
+`;
+
+export const BoothIntro = styled.div`
+  width: 100%;
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  justify-content: space-between;
+
+  gap: 0.8rem;
+  .name {
+    ${(props) => props.theme.fontStyles.basic.subHeadBold};
+    color: ${(props) => props.theme.colors.white};
+  }
+
+  .intro {
+    ${(props) => props.theme.fontStyles.basic.body2Med};
+    color: ${(props) => props.theme.colors.gray60};
+  }
 `;
