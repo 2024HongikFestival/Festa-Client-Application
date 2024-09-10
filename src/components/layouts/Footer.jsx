e05119289e80f84bd969e24cfbfd0cdc54766f29
letthem <suyeon3662@naver.com>
2024-09-10 12:52:35 +0900

    [Refactor] FooterStyles로 스타일 코드 분리

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,5 +1,4 @@
 import { useLocation, useNavigate } from 'react-router-dom';
-import styled, { css } from 'styled-components';
 import mangaeLogo from '@/assets/webps/layouts/mangae.webp';
 import { useTranslation } from 'react-i18next';
 import rightArrow from '@/assets/svgs/makers/rightArrow.svg';
@@ -9,6 +8,7 @@ import mangaeInsta from '@/assets/svgs/makers/mangaeInsta.svg';
 import wdfInsta from '@/assets/svgs/makers/wdfInsta.svg';
 import gaehwaInsta from '@/assets/svgs/makers/gaehwaInsta.svg';
 import flameVideo from '@/assets/videos/flameMainFooter.mp4';
+import * as S from '@/components/layouts/FooterStyles';
 
 export default function Footer() {
   const nav = useNavigate();
@@ -48,32 +48,32 @@ export default function Footer() {
   };
 
   return (
-    <FooterLayout $path={location.pathname}>
+    <S.FooterLayout $path={location.pathname}>
       {showPreviousBtn && (
-        <PreviousBtn onClick={handleGoBack}>
+        <S.PreviousBtn onClick={handleGoBack}>
           <span>{t('layouts.footer.prev')}</span>
-        </PreviousBtn>
+        </S.PreviousBtn>
       )}
       {flameMain && (
-        <VideoContainer>
-          <BackgroundVideo autoPlay loop muted>
+        <S.VideoContainer>
+          <S.BackgroundVideo autoPlay loop muted>
             <source src={flameVideo} type="video/mp4" />
-          </BackgroundVideo>
-        </VideoContainer>
+          </S.BackgroundVideo>
+        </S.VideoContainer>
       )}
       {flame ? (
-        <Flame>
+        <S.Flame>
           <img src={flameLogo} alt="flame" />
-        </Flame>
+        </S.Flame>
       ) : (
-        <Mangae>
+        <S.Mangae>
           <img src={mangaeLogo} alt="mangae" />
-        </Mangae>
+        </S.Mangae>
       )}
-      <Contributor $path={location.pathname}>
+      <S.Contributor $path={location.pathname}>
         <span>{t('layouts.footer.contributors')}</span>
-      </Contributor>
-      <LikelionBtn $path={location.pathname}>
+      </S.Contributor>
+      <S.LikelionBtn $path={location.pathname}>
         <span className="likelion">{t('layouts.footer.likelion')}</span>
         <span onClick={() => handleNavigation('/likelion')} className="toLikelion">
           {t('layouts.footer.toLikelion')}
@@ -83,8 +83,8 @@ export default function Footer() {
           src={flame ? rightArrowGray : rightArrow}
           alt={flame ? 'rightArrowGray' : 'rightArrow'}
         />
-      </LikelionBtn>
-      <GaehwaBtn $path={location.pathname}>
+      </S.LikelionBtn>
+      <S.GaehwaBtn $path={location.pathname}>
         <span className="gaehwa">{t('layouts.footer.gaehwa')}</span>
         <span onClick={() => handleNavigation('/gaehwa')} className="toGaehwa">
           {t('layouts.footer.toGaehwa')}
@@ -94,10 +94,10 @@ export default function Footer() {
           src={flame ? rightArrowGray : rightArrow}
           alt={flame ? 'rightArrowGray' : 'rightArrow'}
         />
-      </GaehwaBtn>
-      <InstaContainer $path={location.pathname}>
+      </S.GaehwaBtn>
+      <S.InstaContainer $path={location.pathname}>
         <span>{t('layouts.footer.instagram')}</span>
-        <Instagrams>
+        <S.Instagrams>
           <a href="https://www.instagram.com/hiufestival_official/" target="_blank" rel="noopener noreferrer">
             <img src={mangaeInsta} alt="mangaeInsta" />
           </a>
@@ -107,241 +107,8 @@ export default function Footer() {
           <a href="https://www.instagram.com/hiu_student_council/" target="_blank" rel="noopener noreferrer">
             <img src={gaehwaInsta} alt="gaehwaInsta" />
           </a>
-        </Instagrams>
-      </InstaContainer>
-    </FooterLayout>
+        </S.Instagrams>
+      </S.InstaContainer>
+    </S.FooterLayout>
   );
 }
-
-const FooterLayout = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  background-color: transparent;
-  overflow: hidden;
-  z-index: 0;
-`;
-
-const PreviousBtn = styled.div`
-  z-index: 100;
-  margin: 0.1rem auto 0;
-  cursor: pointer;
-  width: 10.6rem;
-  height: 4.3rem;
-  border-radius: 5rem;
-  background: rgba(255, 255, 255, 0.1);
-  backdrop-filter: blur(0.4rem);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  position: relative;
-  position: fixed;
-  bottom: 5.2rem;
-  left: 50%;
-  transform: translateX(-50%);
-  transition: bottom 0.3s ease;
-
-  span {
-    color: ${(props) => props.theme.colors.gray5};
-    ${(props) => props.theme.fontStyles.basic.body2Med};
-  }
-
-  /* ::before로 그라데이션 테두리 추가 */
-  &::before {
-    content: '';
-    position: absolute;
-    top: -0.1rem;
-    left: -0.1rem;
-    width: 10.6rem;
-    height: 4.3rem;
-    border-radius: 5rem;
-    padding: 1px; /* 테두리 두께 */
-    background: linear-gradient(180deg, rgba(255, 255, 255, 0.8) 0%, rgba(255, 255, 255, 0.2) 100%);
-    -webkit-mask:
-      linear-gradient(#fff 0 0) content-box,
-      linear-gradient(#fff 0 0);
-    mask:
-      linear-gradient(#fff 0 0) content-box,
-      linear-gradient(#fff 0 0);
-    -webkit-mask-composite: xor;
-    mask-composite: exclude;
-  }
-`;
-const Mangae = styled.div`
-  margin-top: 3.2rem;
-  margin-left: 3.2rem;
-  margin-bottom: 4.8rem;
-  width: 9.85rem;
-  height: 4rem;
-  overflow: hidden;
-  z-index: 1;
-  img {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const Flame = styled.div`
-  margin-top: 2.4rem;
-  margin-left: 2.45rem;
-  margin-bottom: 4rem;
-  width: 11.5rem;
-  height: 3.3rem;
-  overflow: hidden;
-  z-index: 1;
-  img {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const Contributor = styled.div`
-  margin-left: 3.2rem;
-  margin-bottom: 1.2rem;
-  height: 1.8rem;
-  z-index: 1;
-  span {
-    color: ${(props) => props.theme.colors.white};
-    ${(props) => props.theme.fontStyles.basic.captionMed};
-    font-weight: 300;
-  }
-  ${(props) =>
-    props.$path.startsWith('/flame') &&
-    css`
-      span {
-        color: ${(props) => props.theme.colors.gray10};
-      }
-    `};
-`;
-const LikelionBtn = styled.div`
-  margin-left: 3.2rem;
-  display: flex;
-  align-items: center;
-  color: ${(props) => props.theme.colors.white};
-  z-index: 1;
-
-  .likelion {
-    width: 4.9rem;
-    ${(props) => props.theme.fontStyles.basic.body2Bold};
-    margin-right: 2rem;
-    ${(props) =>
-      props.$path.startsWith('/flame') &&
-      css`
-        color: ${(props) => props.theme.colors.gray40};
-      `};
-  }
-  .toLikelion {
-    cursor: pointer;
-    ${(props) => props.theme.fontStyles.basic.body2Med};
-    font-weight: 400;
-    margin-right: 0.6rem;
-    ${(props) =>
-      props.$path.startsWith('/flame') &&
-      css`
-        color: ${(props) => props.theme.colors.gray40};
-      `};
-  }
-  img {
-    cursor: pointer;
-    width: 0.6rem;
-    height: 1rem;
-  }
-`;
-
-const GaehwaBtn = styled.div`
-  margin-left: 3.2rem;
-  margin-top: 1.2rem;
-  margin-bottom: 4rem;
-  display: flex;
-  align-items: center;
-  color: ${(props) => props.theme.colors.white};
-  z-index: 1;
-
-  .gaehwa {
-    width: 4.9rem;
-    ${(props) => props.theme.fontStyles.basic.body2Bold};
-    margin-right: 2rem;
-    ${(props) =>
-      props.$path.startsWith('/flame') &&
-      css`
-        color: ${(props) => props.theme.colors.gray40};
-      `};
-  }
-  .toGaehwa {
-    cursor: pointer;
-    ${(props) => props.theme.fontStyles.basic.body2Med};
-    font-weight: 400;
-    margin-right: 0.6rem;
-    ${(props) =>
-      props.$path.startsWith('/flame') &&
-      css`
-        color: ${(props) => props.theme.colors.gray40};
-      `};
-  }
-  img {
-    cursor: pointer;
-    width: 0.6rem;
-    height: 1rem;
-  }
-`;
-
-const InstaContainer = styled.div`
-  margin-left: 3.2rem;
-  display: flex;
-  flex-direction: column;
-  z-index: 1;
-  span {
-    color: ${(props) => props.theme.colors.white};
-    ${(props) => props.theme.fontStyles.basic.captionMed};
-    font-weight: 300;
-    margin-bottom: 1.15rem;
-    ${(props) =>
-      props.$path.startsWith('/flame') &&
-      css`
-        color: ${(props) => props.theme.colors.gray10};
-      `};
-  }
-
-  a {
-    width: 3.2rem;
-    height: 3.2rem;
-    margin-right: 1.1rem;
-  }
-
-  img {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const Instagrams = styled.div`
-  display: flex;
-  margin-bottom: 6.65rem;
-`;
-
-const VideoContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 35.1rem;
-  pointer-events: none;
-  background-color: transparent;
-  background: linear-gradient(
-    180deg,
-    #010304 10.06%,
-    rgba(1, 3, 4, 0.1) 41.26%,
-    rgba(1, 3, 4, 0.45) 62.62%,
-    #010304 99.93%
-  );
-`;
-
-const BackgroundVideo = styled.video`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  z-index: -1;
-`;
---
+++ src/components/layouts/FooterStyles.js
@@ -0,0 +1,234 @@
+import styled, { css } from 'styled-components';
+
+export const FooterLayout = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  background-color: transparent;
+  overflow: hidden;
+  z-index: 0;
+`;
+
+export const PreviousBtn = styled.div`
+  z-index: 100;
+  margin: 0.1rem auto 0;
+  cursor: pointer;
+  width: 10.6rem;
+  height: 4.3rem;
+  border-radius: 5rem;
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(0.4rem);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: relative;
+  position: fixed;
+  bottom: 5.2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  transition: bottom 0.3s ease;
+
+  span {
+    color: ${(props) => props.theme.colors.gray5};
+    ${(props) => props.theme.fontStyles.basic.body2Med};
+  }
+
+  /* ::before로 그라데이션 테두리 추가 */
+  &::before {
+    content: '';
+    position: absolute;
+    top: -0.1rem;
+    left: -0.1rem;
+    width: 10.6rem;
+    height: 4.3rem;
+    border-radius: 5rem;
+    padding: 1px; /* 테두리 두께 */
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0.8) 0%, rgba(255, 255, 255, 0.2) 100%);
+    -webkit-mask:
+      linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    mask:
+      linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+  }
+`;
+export const Mangae = styled.div`
+  margin-top: 3.2rem;
+  margin-left: 3.2rem;
+  margin-bottom: 4.8rem;
+  width: 9.85rem;
+  height: 4rem;
+  overflow: hidden;
+  z-index: 1;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Flame = styled.div`
+  margin-top: 2.4rem;
+  margin-left: 2.45rem;
+  margin-bottom: 4rem;
+  width: 11.5rem;
+  height: 3.3rem;
+  overflow: hidden;
+  z-index: 1;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Contributor = styled.div`
+  margin-left: 3.2rem;
+  margin-bottom: 1.2rem;
+  height: 1.8rem;
+  z-index: 1;
+  span {
+    color: ${(props) => props.theme.colors.white};
+    ${(props) => props.theme.fontStyles.basic.captionMed};
+    font-weight: 300;
+  }
+  ${(props) =>
+    props.$path.startsWith('/flame') &&
+    css`
+      span {
+        color: ${(props) => props.theme.colors.gray10};
+      }
+    `};
+`;
+export const LikelionBtn = styled.div`
+  margin-left: 3.2rem;
+  display: flex;
+  align-items: center;
+  color: ${(props) => props.theme.colors.white};
+  z-index: 1;
+
+  .likelion {
+    width: 4.9rem;
+    ${(props) => props.theme.fontStyles.basic.body2Bold};
+    margin-right: 2rem;
+    ${(props) =>
+      props.$path.startsWith('/flame') &&
+      css`
+        color: ${(props) => props.theme.colors.gray40};
+      `};
+  }
+  .toLikelion {
+    cursor: pointer;
+    ${(props) => props.theme.fontStyles.basic.body2Med};
+    font-weight: 400;
+    margin-right: 0.6rem;
+    ${(props) =>
+      props.$path.startsWith('/flame') &&
+      css`
+        color: ${(props) => props.theme.colors.gray40};
+      `};
+  }
+  img {
+    cursor: pointer;
+    width: 0.6rem;
+    height: 1rem;
+  }
+`;
+
+export const GaehwaBtn = styled.div`
+  margin-left: 3.2rem;
+  margin-top: 1.2rem;
+  margin-bottom: 4rem;
+  display: flex;
+  align-items: center;
+  color: ${(props) => props.theme.colors.white};
+  z-index: 1;
+
+  .gaehwa {
+    width: 4.9rem;
+    ${(props) => props.theme.fontStyles.basic.body2Bold};
+    margin-right: 2rem;
+    ${(props) =>
+      props.$path.startsWith('/flame') &&
+      css`
+        color: ${(props) => props.theme.colors.gray40};
+      `};
+  }
+  .toGaehwa {
+    cursor: pointer;
+    ${(props) => props.theme.fontStyles.basic.body2Med};
+    font-weight: 400;
+    margin-right: 0.6rem;
+    ${(props) =>
+      props.$path.startsWith('/flame') &&
+      css`
+        color: ${(props) => props.theme.colors.gray40};
+      `};
+  }
+  img {
+    cursor: pointer;
+    width: 0.6rem;
+    height: 1rem;
+  }
+`;
+
+export const InstaContainer = styled.div`
+  margin-left: 3.2rem;
+  display: flex;
+  flex-direction: column;
+  z-index: 1;
+  span {
+    color: ${(props) => props.theme.colors.white};
+    ${(props) => props.theme.fontStyles.basic.captionMed};
+    font-weight: 300;
+    margin-bottom: 1.15rem;
+    ${(props) =>
+      props.$path.startsWith('/flame') &&
+      css`
+        color: ${(props) => props.theme.colors.gray10};
+      `};
+  }
+
+  a {
+    width: 3.2rem;
+    height: 3.2rem;
+    margin-right: 1.1rem;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Instagrams = styled.div`
+  display: flex;
+  margin-bottom: 6.65rem;
+`;
+
+export const VideoContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 35.1rem;
+  pointer-events: none;
+  background-color: transparent;
+  background: linear-gradient(
+    180deg,
+    #010304 10.06%,
+    rgba(1, 3, 4, 0.1) 41.26%,
+    rgba(1, 3, 4, 0.45) 62.62%,
+    #010304 99.93%
+  );
+`;
+
+export const BackgroundVideo = styled.video`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+`;
