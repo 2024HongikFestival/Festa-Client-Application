3dc9b8c0f6d2fa88e9a41ad8a65061e5efc1365c
letthem <suyeon3662@naver.com>
2024-09-12 15:16:36 +0900

    [Feat] 편의시설 화장실 ko.json & en.json

--- src/pages/facilities/ToiletSection.jsx
+++ src/pages/facilities/ToiletSection.jsx
@@ -1,32 +1,39 @@
 import ContentContainer from '@/components/common/ContentContainer';
 import * as S from '@/pages/facilities/styles';
+import { useTranslation } from 'react-i18next';
 
 const ToiletSection = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <ContentContainer>
         <S.Title>
-          화장실 <span>위치</span>
+          {t('facilities.toilet.title1')} <span>{t('facilities.toilet.title2')}</span>
         </S.Title>
         <S.Map />
         <S.ToiletDetail>
           <S.DetailItem>
             <S.Number>1</S.Number>
             <S.GrayBar />
-            <S.Building>M동</S.Building>
-            <S.Floor>1층</S.Floor>
+            <S.Building>{t('facilities.toilet.building1')}</S.Building>
+            <S.Floor>{t('facilities.toilet.floor1')}</S.Floor>
           </S.DetailItem>
           <S.DetailItem>
             <S.Number>2</S.Number>
             <S.GrayBar />
-            <S.Building>Z4동</S.Building>
-            <S.Floor>1층 (장애인 화장실)</S.Floor>
+            <S.Building>
+              {t('facilities.toilet.building2')}
+              <br />
+              {t('facilities.toilet.building2-2')}
+            </S.Building>
+            <S.Floor>{t('facilities.toilet.floor2')}</S.Floor>
           </S.DetailItem>
           <S.DetailItem>
             <S.Number>3</S.Number>
             <S.GrayBar />
-            <S.Building>Z4동</S.Building>
-            <S.Floor>2층</S.Floor>
+            <S.Building>{t('facilities.toilet.building3')}</S.Building>
+            <S.Floor>{t('facilities.toilet.floor3')}</S.Floor>
           </S.DetailItem>
         </S.ToiletDetail>
       </ContentContainer>
