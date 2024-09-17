0a723a10f24998fd8f12720822302c22e87ef186
Cuyqo <saon0410@gmail.com>
2024-09-17 19:04:38 +0900

    [Fix]

--- src/i18n/setting.js
+++ src/i18n/setting.js
@@ -18,7 +18,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en',
+    lng: localStorage.getItem('language') || 'ko',
     fallbackLng: 'ko',
     interpolation: {
       escapeValue: false,
