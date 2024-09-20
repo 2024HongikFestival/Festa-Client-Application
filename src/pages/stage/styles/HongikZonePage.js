b8e54610fbd0d749d06735c148be53c8766f1d0d
dong-wook <boogiewooki02@gmail.com>
2024-09-21 02:13:19 +0900

    [Feat] 홍익존 위치지도 추가

--- src/pages/stage/styles/HongikZonePage.js
+++ src/pages/stage/styles/HongikZonePage.js
@@ -82,9 +82,8 @@ export const LocationTitle = styled.p`
   ${(props) => props.theme.fontStyles.main.headline6};
   color: ${(props) => props.theme.colors.gray100};
 `;
-export const Map = styled.div`
-  width: 33.5rem;
-  height: 25rem;
-  border-top: 1px solid ${(props) => props.theme.colors.gray30};
-  border-bottom: 1px solid ${(props) => props.theme.colors.gray30};
+export const Map = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
