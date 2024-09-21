e4602ba77a052c6d73697878ef6c14c91e4be497
letthem <suyeon3662@naver.com>
2024-09-22 01:55:37 +0900

    [Feat] medical map react-zoom-pan-pinch 라이브러리로 적용

--- src/pages/facilities/MedicalSection.jsx
+++ src/pages/facilities/MedicalSection.jsx
@@ -2,7 +2,7 @@ import ContentContainer from '@/components/common/ContentContainer';
 import * as S from '@/pages/facilities/styles';
 import { useTranslation } from 'react-i18next';
 import medMap from '@/assets/webps/facilities/medMap.webp';
-import DraggableMap from '@/components/facilities/DraggableMap';
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 
 const MedicalSection = () => {
   const { t } = useTranslation();
@@ -14,7 +14,25 @@ const MedicalSection = () => {
           {t('facilities.medical.title1')} <span>{t('facilities.medical.title2')}</span>
         </S.Title>
 
-        <DraggableMap src={medMap} alt="med map" $bottom="-16.5rem" $left="-15.8rem" />
+        <TransformWrapper
+          initialScale={2.1}
+          initialPositionX={-108}
+          initialPositionY={-104}
+          minScale={1}
+          maxScale={1}
+          wheel={{ disabled: true }} // 스크롤 줌 X
+          pinch={{ disabled: true }} // 핀치 줌 X
+          pan={{ disabled: false }} // 팬 기능 O
+          zoomIn={{ disabled: true }} // 줌 인 X
+          zoomOut={{ disabled: true }} // 줌 아웃 X
+          doubleClick={{ disabled: true }}
+        >
+          <S.MapWrapper>
+            <TransformComponent>
+              <S.Map src={medMap} alt="Medical Map" />
+            </TransformComponent>
+          </S.MapWrapper>
+        </TransformWrapper>
 
         <S.Description>
           <span>{t('facilities.medical.description1')}</span>
