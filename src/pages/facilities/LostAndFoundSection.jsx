14a6061c256b70a7cb4115cc4575a15f0c064178
letthem <suyeon3662@naver.com>
2024-09-22 02:01:25 +0900

    [Feat] lost and found map react-zoom-pan-pinch 라이브러리로 적용

--- src/pages/facilities/LostAndFoundSection.jsx
+++ src/pages/facilities/LostAndFoundSection.jsx
@@ -3,7 +3,7 @@ import * as S from '@/pages/facilities/styles';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import lostAndFoundMap from '@/assets/webps/facilities/lostAndFoundMap.webp';
-import DraggableMap from '@/components/facilities/DraggableMap';
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 
 const LostAndFoundSection = () => {
   const nav = useNavigate();
@@ -16,14 +16,25 @@ const LostAndFoundSection = () => {
           {t('facilities.lostAndFound.title1')} <span>{t('facilities.lostAndFound.title2')}</span>
         </S.Title>
 
-        <DraggableMap
-          src={lostAndFoundMap}
-          alt="lost and found map"
-          $bottom="-12.5rem"
-          $left="-20.3rem"
-          $width="96.39rem"
-          $height="62.46rem"
-        />
+        <TransformWrapper
+          initialScale={2.5}
+          initialPositionX={-138}
+          initialPositionY={-248}
+          minScale={1}
+          maxScale={1}
+          wheel={{ disabled: true }} // 스크롤 줌 X
+          pinch={{ disabled: true }} // 핀치 줌 X
+          pan={{ disabled: false }} // 팬 기능 O
+          zoomIn={{ disabled: true }} // 줌 인 X
+          zoomOut={{ disabled: true }} // 줌 아웃 X
+          doubleClick={{ disabled: true }}
+        >
+          <S.MapWrapper>
+            <TransformComponent>
+              <S.Map src={lostAndFoundMap} alt="Lost And Found Map" />
+            </TransformComponent>
+          </S.MapWrapper>
+        </TransformWrapper>
 
         <S.Description>
           <span>{t('facilities.lostAndFound.description1')}</span>
