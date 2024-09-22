e3b935a7dda9cd404eb984bd5aa72825569d1099
서가영 <alstpgml0812@gmail.com>
2024-09-22 17:38:28 +0900

    [Fix] Lostmodal background 이슈 해결

--- src/components/lost-and-found/LostAndFoundPage/LostModal/LostModal.styled.js
+++ src/components/lost-and-found/LostAndFoundPage/LostModal/LostModal.styled.js
@@ -7,8 +7,8 @@ import styled from 'styled-components';
 export const ModalWrapper = styled.div`
   position: fixed;
   top: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background: rgba(0, 0, 0, 0.4);
   z-index: 10;
 
