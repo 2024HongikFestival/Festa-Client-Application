d90bf681b5ee141deae9d000580a8e30d66938b2
ocahs9 <piorag99@gmail.com>
2024-09-22 14:25:56 +0900

    [Chore] 필요없는 주석 제거

--- src/components/lost-and-found/LostAndFoundPage/LostModal/CutomMap.jsx
+++ src/components/lost-and-found/LostAndFoundPage/LostModal/CutomMap.jsx
@@ -1,32 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { createUseGesture, dragAction, pinchAction, useDrag } from '@use-gesture/react';
 import { useSpring, animated } from '@react-spring/web';
-import styled from 'styled-components'; // 혹은 stitches 사용
+import styled from 'styled-components';
 import LostAndFoundLocation from '@/assets/webps/lost/LostAndFoundLocation.webp';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 const MapWrapper = styled.div`
-  width: 33.5rem; // 고정된 너비
-  height: 22.5rem; // 고정된 높이
-  //overflow: hidden; // 컨테이너 밖으로 넘치지 않도록 설정
-  position: relative; // 자식 요소들에 대한 위치 기준
-  border: 1px solid #ddd; // 시각적인 구분을 위해 추가
+  width: 33.5rem;
+  height: 22.5rem;
+  position: relative;
+  border: 1px solid #ddd;
   background-color: #b1daff;
 `;
 
 const LostAndFoundMap = styled(animated.img)`
-  // animated로 이미지 애니메이션 적용
   width: 100%;
   height: 100%;
-  width: 33.5rem; // 고정된 너비
-  height: 22.5rem; // 고정된 높이
+  width: 33.5rem;
+  height: 22.5rem;
 
-  object-fit: cover; // 이미지가 고정된 너비와 높이에 맞게 표시되도록 설정
+  object-fit: cover;
   touch-action: none;
 `;
 
 const CustomMap = () => {
-  const ref = useRef(null); // 이미지 요소에 대한 ref
+  const ref = useRef(null);
 
   return (
     <TransformWrapper initialScale={3.5} initialPositionX={-410} initialPositionY={-550} minScale={1} maxScale={6}>
