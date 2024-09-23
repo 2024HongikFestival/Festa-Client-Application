354cfd9d12a2c65b20f71a97eb0ef4f2ebd1335b
chaeyoungwon <epopcy@naver.com>
2024-09-23 09:44:25 +0900

    [Fix] 이미지 lazy 로딩 추가

---
+++ src/constants/wdfMain/djData.js
@@ -0,0 +1,23 @@
+export const djData = {
+  Day1: [
+    { name: 'RUI', alt: 'DJ 1', loadImage: () => import('@/assets/webps/wdfLineup/rui.webp') },
+    { name: 'FINÈ', alt: 'DJ 2', loadImage: () => import('@/assets/webps/wdfLineup/fine.webp') },
+    { name: 'rightback', alt: 'DJ 3', loadImage: () => import('@/assets/webps/wdfLineup/rightback.webp') },
+    { name: 'CHANXER', alt: 'DJ 4', loadImage: () => import('@/assets/webps/wdfLineup/chanxer.webp') },
+    { name: 'TEZZ', alt: 'DJ 5', loadImage: () => import('@/assets/webps/wdfLineup/tezz.webp') },
+  ],
+  Day2: [
+    { name: 'YongSul', alt: 'DJ 6', loadImage: () => import('@/assets/webps/wdfLineup/yongsul.webp') },
+    { name: 'CHOI', alt: 'DJ 7', loadImage: () => import('@/assets/webps/wdfLineup/choi_s.webp') },
+    { name: 'LOZIC', alt: 'DJ 8', loadImage: () => import('@/assets/webps/wdfLineup/lozic.webp') },
+    { name: 'VANDAL ROCK', alt: 'DJ 9', loadImage: () => import('@/assets/webps/wdfLineup/vandal_rock.webp') },
+    { name: 'ASTER', alt: 'DJ 10', loadImage: () => import('@/assets/webps/wdfLineup/aster_z.webp') },
+  ],
+  Day3: [
+    { name: 'NAP ON CLOUD', alt: 'DJ 11', loadImage: () => import('@/assets/webps/wdfLineup/nap_on_cloud.webp') },
+    { name: 'SIGMA', alt: 'DJ 12', loadImage: () => import('@/assets/webps/wdfLineup/sigma_z.webp') },
+    { name: 'WOOXI', alt: 'DJ 13', loadImage: () => import('@/assets/webps/wdfLineup/wooxi.webp') },
+    { name: 'JOODY', alt: 'DJ 14', loadImage: () => import('@/assets/webps/wdfLineup/joody.webp') },
+    { name: 'JUNCOCO', alt: 'DJ 15', loadImage: () => import('@/assets/webps/wdfLineup/juncoco.webp') },
+  ],
+};
